7224c4e7f0dd6516ef9afa1da6a8d4603132c5d4
MarksonArguello <marksonarguello@gmail.com>
2020-09-09 13:24:40 -0300

    Exercicio 1 pronto

--- src/exercicios/Exercicio1.java
+++ src/exercicios/Exercicio1.java
@@ -1,33 +1,49 @@
+package exercicios;
+
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Exercicio1 {
-    public static void executar(){
-        System.out.print("Digite o tamanho da lista: ");
-        int tamanhoDaLista = lerNumeroInteiro();
-        float[] lista = criarELerLista(tamanhoDaLista);
+    private static Scanner sc = new Scanner(System.in);
 
-
-        float mediaDaLista = calcularMediaLista(tamanhoDaLista, lista);
-
-        System.out.printf("A média da lista é %.2f\n", mediaDaLista);
-    }
-    public static float lerNumeroFloat() {
-        float numero = 0;
+    private static int lerNumeroInteiro() {
+        int numeroInteiro = 0;
         boolean numeroLido = false;
         do{
-            try (Scanner sc = new Scanner(System.in)) {
+            try  {
+
+                numeroInteiro = sc.nextInt();
+                numeroLido = true;
+            }
+            catch (InputMismatchException e){
+                System.out.println("====O número lido não é um inteiro====");
+                sc.next();
+            }
+
+        }while (!numeroLido);
+        return numeroInteiro;
+    }
+
+    private static float lerNumeroFloat() {
+        float numero = 0;
+        boolean numeroLido = false;
+
+        do{
+            try {
                 numero = sc.nextFloat();
                 numeroLido = true;
             }
             catch (InputMismatchException e){
                 System.out.println("====O número lido não é um número real====");
+                sc.next();
             }
+
         }while (!numeroLido);
         return numero;
     }
 
-    public static float[] criarELerLista(int tamanhoDaLista) {
+    private static float[] criarELerLista(int tamanhoDaLista) {
         float[] lista = new float[tamanhoDaLista];
         for (int indice = 0; indice < tamanhoDaLista; ++indice) {
             System.out.printf("Digite o número na posição %d: ", indice + 1);
@@ -37,27 +53,24 @@ public class Exercicio1 {
         return lista;
     }
 
-    public static int lerNumeroInteiro() {
-        int numeroInteiro = 0;
-        boolean numeroLido = false;
-        do{
-            try (Scanner sc = new Scanner(System.in)) {
-                numeroInteiro = sc.nextInt();
-                numeroLido = true;
-            }
-            catch (InputMismatchException e){
-                System.out.println("====O número lido não é um inteiro====");
-            }
-        }while (!numeroLido);
-        return numeroInteiro;
-    }
 
-    public static float calcularMediaLista(int tamanhoDaLista, float[] lista) {
+    private static float calcularMediaLista(int tamanhoDaLista, float[] lista) {
         float media = 0f;
         for (int indice = 0; indice < tamanhoDaLista; ++indice) {
             media += lista[indice];
         }
         media /= tamanhoDaLista;
         return media;
+    }
+
+    public static void executar(){
+
+        System.out.print("Digite o tamanho da lista: ");
+        int tamanhoDaLista = lerNumeroInteiro();
+
+        float[] lista = criarELerLista(tamanhoDaLista);
+        float mediaDaLista = calcularMediaLista(tamanhoDaLista, lista);
+
+        System.out.printf("A média da lista é %.2f\n", mediaDaLista);
     }
 }
